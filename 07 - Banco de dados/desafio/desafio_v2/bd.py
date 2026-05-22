77fb64d5ad46a3f8312c68215cff63eec1e48b60
CleisonMendes <128653613+CleisonMendes@users.noreply.github.com>
2026-05-22 14:56:59 -0300

    Add tables for account and transaction in database

--- 07 - Banco de dados/desafio/desafio_v2/bd.py
+++ 07 - Banco de dados/desafio/desafio_v2/bd.py
@@ -29,10 +29,33 @@ def criar_bd(cursor: Cursor) -> None:
             faturamento_anual REAL NOT NULL,
             FOREIGN KEY (cliente_id) REFERENCES cliente(id)
         );
-                   """
+
+        CREATE TABLE IF NOT EXISTS conta (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            numero INTEGER NOT NULL UNIQUE,
+            agencia TEXT NOT NULL,
+            saldo REAL NOT NULL DEFAULT 0.0,
+            limite REAL NOT NULL,
+            limite_saques INTEGER NOT NULL,
+            cliente_id INTEGER NOT NULL,
+            FOREIGN KEY (cliente_id) REFERENCES cliente(id)
+        );
+
+        CREATE TABLE IF NOT EXISTS transacao (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            tipo TEXT NOT NULL, -- 'deposito' ou 'saque'
+            valor REAL NOT NULL,
+            data DATETIME DEFAULT CURRENT_TIMESTAMP,
+            conta_id INTEGER NOT NULL,
+            FOREIGN KEY (conta_id) REFERENCES conta(id)
+        );
+        """
     )
 
 
 def criar_conexao() -> Connection:
     ROOT_PATH = Path(__file__).parent
-    return sqlite3.connect(ROOT_PATH / "db.sqlite")
+    conexao = sqlite3.connect(ROOT_PATH / "db.sqlite")
+    # ATENÇÃO: Ativar chaves estrangeiras para o SQLite respeitar os relacionamentos das tabelas
+    conexao.execute("PRAGMA foreign_keys = ON;")
+    return conexao
