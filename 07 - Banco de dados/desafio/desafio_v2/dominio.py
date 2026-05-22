f7276f368a255a8721d9cc95a375963bfca27c08
CleisonMendes <128653613+CleisonMendes@users.noreply.github.com>
2026-05-22 14:59:50 -0300

    Refactor Cliente and Pessoa classes
    Removed string representation and database conversion methods from Cliente and PessoaFisica classes. Added faturamento_anual attribute to PessoaJuridica class.

--- 07 - Banco de dados/desafio/desafio_v2/dominio.py
+++ 07 - Banco de dados/desafio/desafio_v2/dominio.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass
-from typing import Self
 
 
 @dataclass
@@ -8,13 +7,6 @@ class Cliente:
     telefone: str
     status: str
 
-    def __str__(self) -> str:
-        texto = ""
-        for campo, valor in self.__dict__.items():
-            campo = campo.replace("_", " ").capitalize()
-            texto += f"{campo}: {valor}\n"
-        return texto
-
 
 @dataclass
 class PessoaFisica(Cliente):
@@ -22,31 +14,9 @@ class PessoaFisica(Cliente):
     cpf: str
     renda_mensal: float
 
-    @classmethod
-    def converter_objeto_bd(cls, objeto_db: dict) -> Self:
-        return cls(
-            email=objeto_db["email"],
-            telefone=objeto_db["telefone"],
-            status=objeto_db["status"],
-            nome=objeto_db["nome"],
-            cpf=objeto_db["cpf"],
-            renda_mensal=objeto_db["renda_mensal"],
-        )
-
 
 @dataclass
 class PessoaJuridica(Cliente):
     nome_fantasia: str
     cnpj: str
     faturamento_anual: float
-
-    @classmethod
-    def converter_objeto_bd(cls, objeto_db: dict) -> Self:
-        return cls(
-            email=objeto_db["email"],
-            telefone=objeto_db["telefone"],
-            status=objeto_db["status"],
-            nome_fantasia=objeto_db["nome_fantasia"],
-            cnpj=objeto_db["cnpj"],
-            faturamento_anual=objeto_db["faturamento_anual"],
-        )
